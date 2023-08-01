4cf4e14789f6f1f0e70e98da763ef7230afa2f52
Cristhiam D. López Nuñez <cristhiam.lopez.nunez@gmail.com>
2023-08-01 14:08:53 -0400

    Added api url service

---
+++ src/app/core/services/api-url.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ApiUrlHelperService {
+
+  private readonly _apiUrl: string;
+
+  constructor() {
+    this._apiUrl = 'https://3q8pjz0cpl.execute-api.us-east-1.amazonaws.com/';
+  }
+
+  get apiUrl(): string {
+    return this._apiUrl;
+  }
+
+  get categoryURL() {
+    return `${this.apiUrl}/api/Category/`;
+  }
+
+  get categorySearchURL() {
+    return `${this.categoryURL}/Search`;
+  }
+
+  get itemURL() {
+    return `${this.apiUrl}/api/Item/`;
+  }
+
+  get itemSearchURL() {
+    return `${this.categoryURL}/Search`;
+  }
+
+
+}

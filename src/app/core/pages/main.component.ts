b6b138ee315181c64b558c5636ed8ad60d0e04c0
Cristhiam D. López Nuñez <cristhiam.lopez.nunez@gmail.com>
2023-08-02 03:50:44 -0400

    Finished categories functionalities

--- src/app/core/pages/main.component.ts
+++ src/app/core/pages/main.component.ts
@@ -1,9 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {BehaviorSubject, Subject} from 'rxjs';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 import {ApiUrlService} from '../services/api-url.service';
 import {HttpClient} from "@angular/common/http";
 import {CategoryInterface} from "../interfaces/category.interface";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
+const slideLeft = 'animate-fade-in-left';
+const slideRight = 'animate-fade-in-right';
 
 @Component({
   selector: 'app-main-page',
@@ -13,15 +16,15 @@ import {CategoryInterface} from "../interfaces/category.interface";
 
 export class MainPageComponent implements OnInit {
 
+  @ViewChild('tabsContainer') applyContainer!: ElementRef<HTMLDivElement>;
+  animationIndex: number = 0;
+
   isUpdating: boolean = false;
+  isDeleting: boolean = false;
   categoryId: number = -1;
+  itemPosition: number = -1;
 
   categories$ = new BehaviorSubject<CategoryInterface[]>([]);
-
-  public categoryObject: CategoryInterface = {
-    name: '',
-    description: '',
-  };
 
   public categoryParams = {
     pageNo: 0,
@@ -29,7 +32,13 @@ export class MainPageComponent implements OnInit {
     filters: null
   };
 
+  readonly category_form = new FormGroup({
+    name: new FormControl<string>('', [Validators.required, Validators.maxLength(20), Validators.minLength(1)]),
+    description: new FormControl<string>('', [Validators.required, Validators.minLength(1)]),
+  });
+
   constructor(
+    private render2: Renderer2,
     private httpClient: HttpClient,
     private apiUrlService: ApiUrlService,
   ) {
@@ -41,39 +50,49 @@ export class MainPageComponent implements OnInit {
   }
 
   validCategoryForm() {
-    return (this.categoryObject.name && this.categoryObject.description)
+    return this.category_form.valid;
   }
 
   manageCategory(isUpdating: boolean, id: number) {
-    if (this.categoryObject.name && this.categoryObject.description) {
+
+    if (this.category_form.valid) {
 
       if (isUpdating && this.categoryId !== -1) {
-        this.httpClient.put(`${this.apiUrlService.categoryURL}/${id}`, this.categoryObject)
+        this.httpClient.put(`${this.apiUrlService.categoryURL}/${id}`, this.category_form.value)
           .subscribe({
-            next: (res) => {
-              console.log(res)
+            next: (res: any) => {
+              if (res) {
+                const array = this.categories$.getValue();
+                array[this.itemPosition] = res;
+                this.categories$.next(array);
+                this.cancelUpdate()
+              }
             },
             error: () => console.log('Error'),
             complete: () => console.log('Complete')
           });
       } else {
-        this.httpClient.post(`${this.apiUrlService.categoryURL}`, this.categoryObject)
+        this.httpClient.post(`${this.apiUrlService.categoryURL}`, this.category_form.value)
           .subscribe({
             next: (res) => {
-              console.log(res)
+              this.loadCategories();
             },
             error: () => console.log('Error'),
             complete: () => console.log('Complete')
           });
       }
+
     }
   }
 
-  deleteCategory(category: CategoryInterface) {
-    this.httpClient.post(`${this.apiUrlService.categoryURL}/${category.id}/Remove`, '')
+  deleteCategory() {
+    this.httpClient.post(`${this.apiUrlService.categoryURL}/${this.categoryId}/Remove`, '')
       .subscribe({
         next: (res) => {
-          console.log(res)
+          const data = this.categories$.getValue();
+          data.splice(this.itemPosition, 1)
+          this.categories$.next(data);
+          this.cancelDelete();
         },
         error: () => console.log('Error'),
         complete: () => console.log('Complete')
@@ -93,22 +112,71 @@ export class MainPageComponent implements OnInit {
   }
 
 
-  toUpdate(category: CategoryInterface) {
+  toUpdate(category: CategoryInterface, pos: number) {
     this.isUpdating = true;
     this.categoryId = category?.id || -1;
-    this.categoryObject = {
+    this.itemPosition = pos || -1;
+    this.category_form.patchValue({
       name: category.name,
       description: category.description,
-    }
+    });
   }
 
-  cancelUpdate(category: CategoryInterface) {
+  cancelUpdate() {
     this.isUpdating = false;
     this.categoryId = -1;
-    this.categoryObject = {
-      name: '',
-      description: '',
+    this.category_form.reset();
+  }
+
+  toDelete(category: CategoryInterface, pos: number) {
+    this.isDeleting = true;
+    this.categoryId = category?.id || -1;
+    this.itemPosition = pos || -1;
+  }
+
+  cancelDelete() {
+    this.isDeleting = false;
+    this.categoryId = -1;
+    this.category_form.reset();
+  }
+
+
+
+
+
+  cycleTabs(tabPosition: number) {
+    if (!(this.animationIndex === tabPosition)) {
+      if (this.animationIndex === 0) {
+        this.animationIndex = 1;
+        this.render2.addClass(this.applyContainer.nativeElement, slideRight);
+        setTimeout(
+          () =>
+            this.render2.removeClass(
+              this.applyContainer.nativeElement,
+              slideRight
+            ),
+          1001
+        );
+      } else if (this.animationIndex === 1) {
+        this.animationIndex = 0;
+        this.render2.addClass(this.applyContainer.nativeElement, slideLeft);
+        setTimeout(
+          () =>
+            this.render2.removeClass(
+              this.applyContainer.nativeElement,
+              slideLeft
+            ),
+          1001
+        );
+      }
     }
   }
 
 }
+
+
+
+
+
+
+
